a0aa5ebcbf0eeb560c71b8512d6e14ca87db84ef
Toby Alden <toby.alden@gmail.com>
2016-09-04 18:04:34 -0700

    Add jetpack

--- src/entities/Player.hx
+++ src/entities/Player.hx
@@ -8,6 +8,7 @@ class Player extends ActiveEntity
 {
 
   public static inline var RUN_SPEED = 1;
+  public static inline var AIR_RUN_SPEED = 0.5;
   public static inline var MAX_RUN_SPEED = 10;
   public static inline var DECCEL_SPEED = 1;
 
@@ -15,21 +16,40 @@ class Player extends ActiveEntity
 
   public static inline var GRAVITY = 10;
   public static inline var MAX_FALL_SPEED = 20;
+  public static inline var MAX_UPWARDS_SPEED = 30;
 
   public static inline var JUMP_POWER = 10;
   public static inline var JUMP_CANCEL_POWER = 5;
 
+  public static inline var BOOST_POWER = 0.6;
+  public static inline var MAX_JETPACK_FUEL = 50;
+
+  private var fuel:Int;
+  private var canBoost:Bool;
+
+  private var fuelBar:Image;
+  private var allGraphics:Graphiclist;
+
   public function new(x:Int, y:Int)
   {
     super(x, y);
+    allGraphics = new Graphiclist(new Array<Graphic>());
     sprite = new Spritemap("graphics/player.png", 30, 30);
     sprite.add("idle", [0]);
     sprite.add("run", [1, 2, 3, 4], 12);
     sprite.add("jump", [5]);
     sprite.add("boost", [6]);
     sprite.add("skid", [7]);
-    graphic = sprite;
+
+    fuelBar = new Image("graphics/fuelbar.png");
+    fuelBar.alpha = 0.5;
+
+    allGraphics.add(fuelBar);
+    allGraphics.add(sprite);
+    graphic = allGraphics;
     setHitbox(16, 16, -7, -14);
+    fuel = MAX_JETPACK_FUEL;
+    canBoost = false;
   }
 
   public override function update()
@@ -41,8 +61,16 @@ class Player extends ActiveEntity
         velocity.x = 0;
       }
 
+      var runSpeed:Float;
+      if(isOnGround()) {
+        runSpeed = RUN_SPEED;
+      }
+      else {
+        runSpeed = AIR_RUN_SPEED;
+      }
+
       if(Input.check(Key.LEFT)) {
-        velocity.x = Math.max(velocity.x - RUN_SPEED, -MAX_RUN_SPEED);
+        velocity.x = Math.max(velocity.x - runSpeed, -MAX_RUN_SPEED);
         if(velocity.x > 0) {
           sprite.play("skid");
         }
@@ -52,7 +80,7 @@ class Player extends ActiveEntity
         sprite.flipped = true;
       }
       else if(Input.check(Key.RIGHT)) {
-        velocity.x = Math.min(velocity.x + RUN_SPEED, MAX_RUN_SPEED);
+        velocity.x = Math.min(velocity.x + runSpeed, MAX_RUN_SPEED);
         if(velocity.x < 0) {
           sprite.play("skid");
         }
@@ -85,6 +113,8 @@ class Player extends ActiveEntity
 
       if(isOnGround()) {
         velocity.y = 0;
+        canBoost = false;
+        fuel = MAX_JETPACK_FUEL;
         if(Input.check(Key.Z)) {
           velocity.y = -JUMP_POWER;
         }
@@ -94,13 +124,29 @@ class Player extends ActiveEntity
         if(isOnCeiling()) {
           velocity.y = JUMP_POWER/5;
         }
-        else if(Input.released(Key.Z) && velocity.y < -JUMP_CANCEL_POWER) {
+        if(velocity.y > -JUMP_CANCEL_POWER  && !Input.check(Key.Z)) {
+          canBoost = true;
+        }
+        else if(Input.released(Key.Z) && velocity.y < -JUMP_CANCEL_POWER && !canBoost) {
           velocity.y = -JUMP_CANCEL_POWER;
+          canBoost = true;
+        }
+        if(canBoost && Input.check(Key.Z)) {
+          if(fuel > 0)
+          {
+            velocity.y -= BOOST_POWER;
+            fuel -= 1;
+            sprite.play("boost");
+          }
         }
         velocity.y = Math.min(velocity.y + GRAVITY * HXP.elapsed, MAX_FALL_SPEED);
+        velocity.y = Math.max(velocity.y, -MAX_UPWARDS_SPEED);
       }
 
       moveBy(velocity.x, velocity.y , "walls");
+
+      fuelBar.scaleX = fuel / MAX_JETPACK_FUEL;
+      fuelBar.visible = fuel < MAX_JETPACK_FUEL;
   }
 
 }
